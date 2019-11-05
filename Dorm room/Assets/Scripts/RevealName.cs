3b47a111ce842929f847b63bfaf59b1a4cabe375
Jason Bennett <jasoben@gmail.com>
2019-11-05 15:25:34 -0500

    fixed grabbing

---
+++ Dorm room/Assets/Scripts/RevealName.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevealName : MonoBehaviour
+{
+    private GameObject canvas; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = transform.Find("Canvas").gameObject;
+        canvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canvas.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canvas.SetActive(false);
+    }
+}
